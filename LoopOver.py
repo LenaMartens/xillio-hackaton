9b157e22d562988d628ad47581c4bd48e72f7ce3
Taghi Aliyev <taghialiyev@gmail.com>
2016-10-08 16:48:10 +0200

    Updated script and looping over script for finding most common extension

---
+++ LoopOver.py
@@ -0,0 +1,29 @@
+import sys
+import itertools
+import operator
+
+def most_common(L):
+  # get an iterable of (item, iterable) pairs
+  SL = sorted((x, i) for i, x in enumerate(L))
+  # print 'SL:', SL
+  groups = itertools.groupby(SL, key=operator.itemgetter(0))
+  # auxiliary function to get "quality" for an item
+  def _auxfun(g):
+    item, iterable = g
+    count = 0
+    min_index = len(L)
+    for _, where in iterable:
+      count += 1
+      min_index = min(min_index, where)
+    # print 'item %r, count %r, minind %r' % (item, count, min_index)
+    return count, -min_index
+  # pick the highest-count/earliest item
+  return max(groups, key=_auxfun)[0]
+
+if __name__ == '__main__':
+
+    if (len(sys.argv) <= 1):
+            exit(1);
+
+    list = sys.argv[1:];
+    print(most_common(list));
