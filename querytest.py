dd21d596eaa041967a4f07195a32bfb5d895d1fb
Dries De Rydt <dries@e-nventorslab.com>
2016-10-08 11:04:27 +0200

    added google auth, working count query

---
+++ querytest.py
@@ -0,0 +1,34 @@
+from bigquery import get_client
+import time
+#DRIES VERSION
+
+# BigQuery project id as listed in the Google Developers Console.
+project_id = 'rational-diode-145807'
+
+# Service account email address as listed in the Google Developers Console.
+service_account = 'querymachine@rational-diode-145807.iam.gserviceaccount.com'
+
+# PKCS12 or PEM key provided by Google.
+key = 'key.p12'
+#notasecret
+
+client = get_client(project_id, service_account=service_account,
+                    private_key_file=key, readonly=True)
+
+# JSON key provided by Google
+json_key = 'key.json'
+
+client = get_client(json_key_file=json_key, readonly=True)
+
+# Submit an async query.
+job_id, _results = client.query('SELECT COUNT(*) FROM [bigquery-public-data:github_repos.commits]')
+
+# Check if the query has finished running.
+complete, row_count = client.check_job(job_id)
+
+time.sleep(5)
+
+# Retrieve the results.
+results = client.get_query_rows(job_id)
+
+print(results)
