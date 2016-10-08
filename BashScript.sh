8c0dc6e2aa7b9195c23e2c83eda873da90e91570
Taghi Aliyev <taghialiyev@gmail.com>
2016-10-08 14:57:23 +0200

    Simplified the bash script. Next is fetching git information

--- BashScript.sh
+++ BashScript.sh
@@ -1,9 +1,5 @@
-start() {
-    # Sample code to make sure that code within the if .. fi is executed
-    if [ 1 -eq 1 ]; then
-        echo "Start server"
-        /home/taghi/Desktop/Python-3.5.1/python ./querytest.py
-    fi;
-}
-
-start
+# Sample code to make sure that code within the if .. fi is executed
+if [ 1 -eq 1 ]; then
+	echo "Start server"
+	/home/taghi/Desktop/Python-3.5.1/python ./querytest.py
+fi;
