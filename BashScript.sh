458e4d0173ab7e4da6f2623de3fd07b4fa47d887
Taghi Aliyev <taghialiyev@gmail.com>
2016-10-08 14:54:15 +0200

    Example of a bash script that can call a python script and print the outputs on terminal. String can also be caught in order to be used later or pushed somewhere else

---
+++ BashScript.sh
@@ -0,0 +1,9 @@
+start() {
+    # Sample code to make sure that code within the if .. fi is executed
+    if [ 1 -eq 1 ]; then
+        echo "Start server"
+        /home/taghi/Desktop/Python-3.5.1/python ./querytest.py
+    fi;
+}
+
+start
